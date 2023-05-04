c70125404496ebf945b5e989c9d0932daadaf4d2
Dhakshina Perera <dhakshinaperera330@gmail.com>
2023-05-04 22:42:50 +0530

    DBStudent.java

--- DBStudent.java
+++ DBStudent.java
@@ -8,7 +8,7 @@ public class DBStudent {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", null);
             Statement sDB = conn.createStatement();
             sDB.execute(
-                    "Create Table if not exists Student_details(StudentID int primary key auto_increment, Student_Name varchar(300),Student_Address varchar(300),Student_City(300),Student_mail varchar(300),Student_ContactNumber int)");
+                   "Create Table if not exists Student_details(StudentID int primary key auto_increment, Student_Name varchar(300),Student_Address varchar(300),Student_City varchar(300),Student_mail varchar(300),Student_ContactNumber int
             sDB.execute(
                     "Create Table if not exists Student_Course(StudentID int, CourseID int , constraint FK_Student_Course foreign key(StudentID,CourseID) references Student_details(StudentID,CourseID))");
             // Alert connectAlert = new Alert(AlertType.INFORMATION);
